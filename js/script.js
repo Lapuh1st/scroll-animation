630dd00901a38e85d204ba79db5dff86ef593593
Lapuh1st <slavik.lopush@gmail.com>
2015-07-28 21:44:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// var wow = new WOW(
+//     {
+//       boxClass:     'wow',      // default
+//       animateClass: 'animated', // default
+//       offset:       100,          // default
+//       mobile:       true,       // default
+//       live:         true        // default
+//     }
+//   )
+// wow.init();
+
+$(document).ready(function(){
+	// var a = $('.sectionCompany-title');
+	// var b = $('.test');
+	// var $c = $('.sectionHistory-item');
+	makeAnimations();
+
+	$( window ).resize(function(){
+		makeAnimations();
+	});
+
+	function makeAnimations (){
+		var titlePosition = $('.sectionCompany-title').offset().top;
+
+		$( window ).scroll(function() {
+			$('.js-slide-left').each(function(){
+				var elementPosition = $(this).offset().top;
+				var topOfWindow = $(window).scrollTop();
+
+				if (elementPosition < topOfWindow+400) {
+					$(this).addClass("slideLeft");
+				}
+			});
+		});	
+	};
+	
+
+	// prototype script
+
+	// $(window).scroll(function() {
+	// 	$('#animatedElement').each(function(){
+	// 	var imagePos = $(this).offset().top;
+
+	// 	var topOfWindow = $(window).scrollTop();
+	// 		if (imagePos < topOfWindow+400) {
+	// 			$(this).addClass("slideUp");
+	// 		}
+	// 	});
+	// });
+});
