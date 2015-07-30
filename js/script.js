d18544e6dadf7b8485db08a655e81945a3d2e656
Iaroslav Lopushanskyi <iaroslavl@similarweb.com>
2015-07-30 17:26:21 +0300

    adding animation on page

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-	makeAnimations();
-
-	$( window ).resize(function(){
+	if(($( window ).width()) > 1024) {
 		makeAnimations();
-	});
+	}
+
+	var viewport = 800;
+	if(($( window ).width()) < 1367){
+		viewport = 400;
+		var mobile = true;
+	}
 
 	function makeAnimations (){
-		var titlePosition = $('.sectionCompany-title').offset().top;
 
 		$( window ).scroll(function() {
 			$('.js-hidden').each(function(){
 				var elementPosition = $(this).offset().top;
 				var topOfWindow = $(window).scrollTop();
 				
-				if (elementPosition < topOfWindow+800) {
+				if (elementPosition < topOfWindow + viewport) {
 					$(this).addClass("js-show-hidden");
 					$(this).removeClass("js-hidden");
 				}
@@ -21,19 +24,26 @@ $(document).ready(function(){
 			$('.js-hidden-line').each(function(){
 				var elementPosition = $(this).offset().top;
 				var topOfWindow = $(window).scrollTop();
-
-				if (elementPosition < topOfWindow+800) {
-					$(this).addClass("js-show-hidden-line");
+				var self = $(this);
+				if (elementPosition < topOfWindow + viewport) {
+					if($(this).hasClass('sectionHistory-line')){
+						$('.sectionCompany').addClass('js-after');
+						setTimeout(function(){
+							self.addClass("js-show-hidden-line");
+						}, 500);
+					} else{
+						$(this).addClass("js-show-hidden-line");
+					}
 				}
 			});
 			$('.upperLine').each(function(){
 				var elementPosition = $(this).offset().top;
 				var topOfWindow = $(window).scrollTop();
 
-				if (elementPosition < topOfWindow+800) {
+				if (elementPosition < topOfWindow + viewport) {
 					var self = $(this);
 					$(this).addClass("js-upperLine");
-						
+
 					if(self.hasClass('js-first-circles')){	
 						setTimeout(function(){
 							self.next().next().addClass("js-show-circle");
@@ -64,7 +74,6 @@ $(document).ready(function(){
 							}, 600);
 						}, 450);
 					}
-
 				}
 			});
 		});	
