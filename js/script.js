999a4256a6eceb29e9fde8335c44f02f42bb40cb
Iaroslav Lopushanskyi <iaroslavl@similarweb.com>
2015-08-19 10:42:26 +0300

    refactoring code, better animation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-$(document).ready(function () {
-  if (($(window).width()) > 1024) {
-    makeAnimations();
-  }
-
-  var fromTop = 600;
-  if (($(window).width()) < 1367) {
-    fromTop = 500;
-  }
-
+$(window).load(function() {
+  var isMobile = false;
+  var fromTop = $(window).height()/1.45;
   var after = 'js-after';
   var showCircle = 'js-show-circle';
+  var clients = $('.sectionCompany-clients');
+  var showBlock = 'js-show-block';
+  var historyItem = 'sectionHistory-item';
 
   var animations = [
     {
@@ -27,116 +23,86 @@ $(document).ready(function () {
     }
   ];
 
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    isMobile = true;
+  }
+  if (!isMobile) {
+    makeAnimations();
+  }
 
+  function isMobileMode() {
+    var matchMedia;
+    matchMedia = window.matchMedia || window.msMatchMedia;
+    if( matchMedia ) {
+      return matchMedia("(max-width: 1120px)").matches;
+    } else {
+      return $(window).width() <= 1120;
+    }
+  };
+
+  if(isMobileMode()){
+    $.each(animations, function(i) {
+      $('.' + animations[i].element).addClass(animations[i].type);
+    });
+    clients.addClass(after);
+    $(".sectionCompany-circle").addClass(showCircle).addClass(after);
+    $('.' + historyItem).addClass(showBlock);
+  }
 
   function basicAnimation(options) {
     $('.' + options.element).each(function () {
       var elementPosition = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
-
       if (elementPosition < topOfWindow + fromTop) {
         $(this).addClass(options.type);
         $(this).removeClass(options.element);
         var self = $(this);
 
-
-        if($(this).hasClass("sectionCompany-upperLine")) {
+        if($(this).hasClass('sectionCompany-upperLine')) {
           var firstCircle = self.next().next();
           var secondCircle = self.next();
-
-          if(!($(this).hasClass("js-first-circles"))){
+          if(!($(this).hasClass(options.condition))){
             firstCircle = self.next();
             secondCircle = self.next().next();
           }
+          setTimeout(function () {
+            firstCircle.addClass(showCircle);
             setTimeout(function () {
-              firstCircle.addClass(showCircle);
-              setTimeout(function () {
-                firstCircle.addClass(after);
-              }, 450);
-              setTimeout(function () {
-                secondCircle.addClass(showCircle);
-                setTimeout(function () {
-                  secondCircle.addClass(after);
-                  if(!(self.hasClass("js-first-circles"))) {
-                    setTimeout(function () {
-                      $('.sectionCompany-clients').addClass(after);
-                    }, 1400);
-                  }
-                }, 600);
-              }, 600);
+              firstCircle.addClass(after);
             }, 450);
+            setTimeout(function () {
+              secondCircle.addClass(showCircle);
+              setTimeout(function () {
+                secondCircle.addClass(after);
+                if(!(self.hasClass(options.condition))) {
+                  setTimeout(function () {
+                    clients.addClass(after);
+                  }, 1400);
+                }
+              }, 600);
+            }, 600);
+          }, 450);
+        }
+
+        if ($(this).hasClass(historyItem)) {
+          self = $(this);
+          setTimeout(function () {
+            self.addClass(showBlock);
+          }, 750);
+
         }
       }
     });
   };
 
   function makeAnimations() {
-
+    animations.forEach(function (animation) {
+      basicAnimation(animation);
+    });
     $(window).scroll(function () {
-
       animations.forEach(function (animation) {
         basicAnimation(animation);
       });
-
-      //$('.js-hidden').each(function () {
-      //  var elementPosition = $(this).offset().top;
-      //  var topOfWindow = $(window).scrollTop();
-      //
-      //  if (elementPosition < topOfWindow + fromTop) {
-      //    $(this).addClass("js-show-hidden");
-      //    $(this).removeClass("js-hidden");
-      //  }
-      //});
-      //
-      //$('.js-hidden-line').each(function () {
-      //  var elementPosition = $(this).offset().top;
-      //  var topOfWindow = $(window).scrollTop();
-      //
-      //  if (elementPosition < topOfWindow + fromTop) {
-      //    $('.sectionCompany').addClass(after);
-      //    $(this).addClass("js-show-hidden-line");
-      //  }
-      //});
-      //$('.upperLine').each(function () {
-      //  var elementPosition = $(this).offset().top;
-      //  var topOfWindow = $(window).scrollTop();
-      //
-      //  if (elementPosition < topOfWindow + fromTop) {
-      //    var self = $(this);
-      //    $(this).addClass("js-upperLine");
-      //
-      //    if (self.hasClass('js-first-circles')) {
-      //      setTimeout(function () {
-      //        self.next().next().addClass("js-show-circle");
-      //        setTimeout(function () {
-      //          self.next().next().addClass(after);
-      //        }, 450);
-      //        setTimeout(function () {
-      //          self.next().addClass("js-show-circle");
-      //          setTimeout(function () {
-      //            self.next().addClass(after);
-      //          }, 600);
-      //        }, 600);
-      //      }, 450);
-      //    } else {
-      //      setTimeout(function () {
-      //        self.next().addClass("js-show-circle");
-      //        setTimeout(function () {
-      //          self.next().addClass(after);
-      //        }, 450);
-      //        setTimeout(function () {
-      //          self.next().next().addClass("js-show-circle");
-      //          setTimeout(function () {
-      //            self.next().next().addClass(after);
-      //            setTimeout(function () {
-      //              $('.sectionCompany-clients').addClass(after);
-      //            }, 1400);
-      //          }, 600);
-      //        }, 600);
-      //      }, 450);
-      //    }
-      //  }
-      //});
     });
   };
 });
